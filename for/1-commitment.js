835ae6e41ba122da18750b46702b69eb8ae750b3
prantonub <prantonub.cse23@gmail.com>
2025-06-19 17:03:41 +0600

    task added

--- for/1-commitment.js
+++ for/1-commitment.js
@@ -1,7 +1,6 @@
-/***
-
-"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-
+/*/
+"I will invest at least 6 hrs every single day for next 60 days!"
+ this message 60 times. So display this.
  */
 
 for (let i = 1; i <= 60; i++) {
